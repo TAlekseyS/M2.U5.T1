14c47b54c72676ddf4e005a2a088bdc701741f52
Aleksey_T <talekseys@mail.ru>
2023-08-09 18:10:36 +0300

    Rabotayet

--- src/test/java/CardDataTests.java
+++ src/test/java/CardDataTests.java
@@ -1,5 +1,6 @@
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 
@@ -9,12 +10,11 @@ import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 
-
 public class CardDataTests {
 
     @Test
-    void shouldTestCardDelivery()  {
-        open("http://localhost:9999");
+    void shouldTestCardDelivery() {
+        /*   open("http://localhost:9999");
         // $$(".form")
         // SelenideElement form = $("[action=/]");
         // form.$("[placeholder=Город] input").setValue("Казань");
@@ -25,25 +25,29 @@ public class CardDataTests {
         $("[class=checkbox__box]").click();
         $("[class=button__content]").click();
         $(withText("Успешно")).shouldBe(visible, Duration.ofSeconds(15));
+        */
+        open("http://localhost:9999");
+        $("[placeholder=Город]").setValue("Казань");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue("15.08.2023");
+        $("[name=name]").setValue("Кирилл Костиев");
+        $("[name=phone]").setValue("+77987897897");
+        $("[class=checkbox__box]").click();
+        $("[class=button__content]").click();
+         $(withText("Успешно")).shouldBe(visible, Duration.ofSeconds(15));
+       // $("[placeholder=Город]").setValue("Казань");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue("16.08.2023");
+       // $("[name=name]").setValue("Кирилл Костиев");
+        //$("[name=phone]").setValue("+77987898887");
+        // $("[class=checkbox__box]").click();
+        $("[class=button__content]").click();
+        $("[class=button__content]").click();
+        $(withText("Успешно")).shouldBe(visible, Duration.ofSeconds(15));
 
 
-
-
-
-
-
-
-        //Thread.sleep(1000);
 
     }
-
-
-
-
-
-
-
-
 
 
 }
